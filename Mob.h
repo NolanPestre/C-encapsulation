60ef5e6b686d983eca28056f413c3bb1934df1eb
NolanPestre <147041473+NolanPestre@users.noreply.github.com>
2024-10-13 21:30:17 +0200

    Update Mob.h

--- Mob.h
+++ Mob.h
@@ -1,18 +1,12 @@
 #pragma once
-#include "Vecteur2.h"
+#include "Vector2.h"
 #include "Entity.h"
 #include "Alive.h"
 #include "AMovable.h"
-
-
-
-
-class Mob:public Entity, public Alive, public AMovable
+class Mob : public Entity , public Alive , public AMovable , public Vector2
 {
-protected:
-	Vecteur2 v;
-
 public:
-	Mob(float x, float y, float HpMax, float HpNow, Vecteur2 deplacer);
+	Mob(float x, float y, float hpmax, float hp, Vector2 direction, float s);
+	virtual void TakeDamage(float damage) override;
+	virtual void Move(Vector2 direction, float s) override;
 };
-
