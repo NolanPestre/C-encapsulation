10c4f25fe10fad76763ec3c847343aa8186a4f99
NolanPestre <147041473+NolanPestre@users.noreply.github.com>
2024-10-13 21:54:03 +0200

    Update Mob.cpp

--- Mob.cpp
+++ Mob.cpp
@@ -16,7 +16,7 @@ void Mob::TakeDamage(float damage)
 	}
 }
 
-void Mob::Move(Vector2 direction, float s)
+void Mob::deplacer(Vector2 direction, float s)
 {
 	Vector2 newpos (direction.GetX() * s + GetX (), direction.GetY() * s + GetY());
 	std::cout << " Mob move to x = " << newpos.GetX() << " and y = " << newpos.GetY() << std::endl;
