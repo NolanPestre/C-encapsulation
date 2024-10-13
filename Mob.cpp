7d8ec982f014c4792d9e12d5191fa787271ed8e4
NolanPestre <147041473+NolanPestre@users.noreply.github.com>
2024-10-13 21:51:59 +0200

    Update Mob.cpp

--- Mob.cpp
+++ Mob.cpp
@@ -1,9 +1,24 @@
 #include "Mob.h"
+#include <iostream>
 
-Mob::Mob(float x, float y, float HpMax, float HpNow, Vecteur2 deplacement) : Alive(HpMax), AMovable( ),
+
+Mob::Mob(float x, float y, float hpmax, float hp, Vector2 direction, float s) : Alive (hpmax, hp) , AMovable (direction, s) , Vector2(x,y), Entity (Vector2(x,y))
 {
-	SetPosXY(x, y);
-	LifeMax = HpMax;
-	LifeNow = HpNow;
-	deplacement = deplacer();
+	std::cout << " Mob just created at x = " << GetPosX() << " and y = " << GetPosY() << " with maxlife = " << GetLifeM() << " and actuallife = " << GetLifeN() << " with direction x = " << direction.GetX() << " and y = " << direction.GetY () << std::endl;
+}
+
+void Mob::TakeDamage(float damage)
+{
+	HP -= damage;
+	if (HP <= 0)
+	{
+		std::cout << " Mob just died " << std::endl;
+	}
+}
+
+void Mob::Move(Vector2 direction, float s)
+{
+	Vector2 newpos (direction.GetX() * s + GetX (), direction.GetY() * s + GetY());
+	std::cout << " Mob move to x = " << newpos.GetX() << " and y = " << newpos.GetY() << std::endl;
+	direction = newpos;
 }
